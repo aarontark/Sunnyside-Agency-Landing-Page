bf7932640339b7118f9e84a268029c5a403c2c06
aarontark <tarkington.aaron@gmail.com>
2024-06-26 23:33:44 -0500

    finished first half of media query

---
+++ app.js
@@ -0,0 +1,10 @@
+const imgList = document.querySelectorAll('img');
+
+if(window.innerWidth >= 750) {
+    imgList.forEach((img) => {
+        if(img.src.includes('mobile')){
+            img.src = img.src.replace('mobile', 'desktop');
+            console.log(img.src);
+        }
+    })
+}
