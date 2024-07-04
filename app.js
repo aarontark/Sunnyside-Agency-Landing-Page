ac24a3a8412b87d92dbd9aef5e0cf1a8579fd609
aarontark <tarkington.aaron@gmail.com>
2024-07-04 01:37:44 -0500

    added code for navbar in javascript

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const imgList = document.querySelectorAll('img');
+const hamburgerBtn = document.querySelector('.hamburger-img');
+const navCorner = document.querySelector('.nav-corner');
+const navBtns = document.querySelector('.nav-btns');
+
+hamburgerBtn.addEventListener('click', () => {
+    if(navCorner.style.display === 'none' || navCorner.style.display == '') {
+        navCorner.style.display = 'block';
+        navBtns.style.display = 'flex';
+    } else {
+        navCorner.style.display = 'none';
+        navBtns.style.display = 'none';
+    }
+})
 
 if(window.innerWidth >= 750) {
     imgList.forEach((img) => {
         if(img.src.includes('mobile')){
             img.src = img.src.replace('mobile', 'desktop');
-            console.log(img.src);
         }
     })
 }
